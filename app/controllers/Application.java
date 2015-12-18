e6aac97395089e8be3d7e0b70fe29c47350ff2d0
Kasper Saaby <kdsaaby@gmail.com>
2015-12-18 11:37:12 +0100

    Stuff

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,50 @@
 package controllers;
 
+import play.libs.F.Callback;
+import play.libs.F.Callback0;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.hello.hello;
+import play.mvc.WebSocket;
+import utilities.FileLoader;
 import views.html.index;
 
 public class Application extends Controller {
 
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        FileLoader fileLoader = new FileLoader("workspace");
+        String result = fileLoader.Load("bom.vdmsl");
+        return ok(index.render(result));
     }
 
-    public Result hello() {
-        return ok(hello.render("Hello"));
+    public WebSocket<String> hello() {
+        System.out.println("Here!!!");
+        return new WebSocket<String>() {
+
+            // Called when the Websocket Handshake is done.
+            public void onReady(WebSocket.In<String> in, WebSocket.Out<String> out) {
+
+                // For each event received on the socket,
+                in.onMessage(new Callback<String>() {
+                    public void invoke(String event) {
+
+                        // Log events to the console
+                        System.out.println(event);
+
+                    }
+                });
+
+                // When the socket is closed.
+                in.onClose(new Callback0() {
+                    public void invoke() {
+
+                        System.out.println("Disconnected!");
+
+                    }
+                });
+
+                // Send a single 'Hello!' message
+                out.write("Hello!");
+            }
+        };
     }
 }
---
+++ app/controllers/vfs.java
@@ -0,0 +1,170 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemException;
+import org.apache.commons.vfs2.FileType;
+import org.apache.commons.vfs2.impl.StandardFileSystemManager;
+import play.libs.Json;
+import play.mvc.Http;
+import play.mvc.Result;
+
+import java.io.*;
+import java.util.*;
+
+public class vfs extends Application {
+
+    public Result appendFile(String account, String absPath) {
+        Http.RequestBody body = request().body();
+
+        StandardFileSystemManager fsManager;
+        PrintWriter pw = null;
+        OutputStream out;
+
+        String default_ws = "workspace";
+
+        try {
+            fsManager = new StandardFileSystemManager();
+            fsManager.init();
+
+            File file = new File(default_ws);
+            String path = "file://" + file.getAbsolutePath() + "/" + account + "/" + absPath;
+
+            FileObject fileObject = fsManager.resolveFile(path);
+
+            // if the file does not exist, this method creates it, and the parent folder, if necessary
+            // if the file does exist, it appends whatever is written to the output stream
+            out = fileObject.getContent().getOutputStream(true);
+
+            pw = new PrintWriter(out);
+            pw.write(body.asText());
+            pw.flush();
+
+            fileObject.close();
+            fsManager.close();
+        } catch (FileSystemException e) {
+            e.printStackTrace();
+        } finally {
+            if (pw != null)
+                pw.close();
+        }
+
+        return ok();
+    }
+
+    public Result readFile(String account, String absPath) {
+        String default_ws = "workspace";
+        String result = "";
+
+        try {
+            StandardFileSystemManager fsManager = new StandardFileSystemManager();
+            fsManager.init();
+
+            File file = new File(default_ws);
+            String path = "file://" + file.getAbsolutePath() + "/" + account + "/" + absPath;
+
+            FileObject fileObject = fsManager.resolveFile(path);
+
+            if (!fileObject.exists())
+                return ok();
+
+            InputStream inputStream = fileObject.getContent().getInputStream();
+            result = IOUtils.toString(inputStream);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return ok(result);
+    }
+
+    private ObjectNode mapToJson(FileObject fileObject) throws FileSystemException {
+        ObjectNode jsonObject = Json.newObject();
+        jsonObject.put("name", fileObject.getName().toString());
+        jsonObject.put("size", fileObject.getContent().getSize());
+        return jsonObject;
+    }
+
+    public Result readdir(String account, String absPath, String dept) {
+        int dirDept = Integer.parseInt(dept);
+        String default_ws = "workspace";
+        List<FileObject> fileObjects = new ArrayList<FileObject>();
+
+//        List<ObjectNode> jsonList = new ArrayList<>();
+
+        try {
+            StandardFileSystemManager fsManager = new StandardFileSystemManager();
+            fsManager.init();
+
+            File file = new File(default_ws);
+            String path = "file://" + file.getAbsolutePath() + "/" + account + "/" + absPath;
+
+            FileObject fileObject = fsManager.resolveFile(path);
+
+            if (fileObject.getType() == FileType.FOLDER) {
+                fileObjects.add(fileObject);
+
+//                jsonList.add(mapToJson(fileObject));
+
+                Queue<FileObject> dirs = new LinkedList<>();
+                Collections.addAll(dirs, fileObject.getChildren());
+
+                int deptCount = 0;
+                while (!dirs.isEmpty()) {
+                    FileObject fo = dirs.remove();
+                    fileObjects.add(fo);
+//                    jsonList.add(mapToJson(fileObject));
+                    if (fo.getType() == FileType.FOLDER && dirDept > deptCount) {
+                        deptCount++;
+                        Collections.addAll(dirs, fo.getChildren());
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return ok(fileObjects.toString());
+    }
+
+    public Result writeFile(String account, String absPath) {
+        Http.RequestBody body = request().body();
+
+        StandardFileSystemManager fsManager;
+        PrintWriter pw = null;
+        OutputStream out;
+
+        String default_ws = "workspace";
+
+        try {
+            fsManager = new StandardFileSystemManager();
+            fsManager.init();
+
+            File file = new File(default_ws);
+            String path = "file://" + file.getAbsolutePath() + "/" + account + "/" + absPath;
+
+            FileObject fileObject = fsManager.resolveFile(path);
+
+            if (fileObject.exists())
+                fileObject.delete();
+
+            out = fileObject.getContent().getOutputStream(true);
+
+            pw = new PrintWriter(out);
+            pw.write(body.asText());
+            pw.flush();
+
+            fileObject.close();
+            fsManager.close();
+        } catch (FileSystemException e) {
+            e.printStackTrace();
+        } finally {
+            if (pw != null)
+                pw.close();
+        }
+
+
+        return ok();
+    }
+}
