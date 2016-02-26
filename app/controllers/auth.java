411238179b32e36ff1fba645ca3b8bff6877ae97
Kasper Saaby <kdsaaby@gmail.com>
2016-02-26 23:12:09 +0100

    OAuth with google identity

--- app/controllers/auth.java
+++ app/controllers/auth.java
@@ -3,27 +3,31 @@ package controllers;
 import com.google.identitytoolkit.GitkitClient;
 import com.google.identitytoolkit.GitkitClientException;
 import com.google.identitytoolkit.GitkitUser;
-import play.mvc.Http;
+import core.StatusCode;
 import play.mvc.Result;
 
 import java.io.IOException;
 
 public class auth extends Application {
 
-    public Result authCallback() {
+    public Result authCallback(String token) {
         String userInfo = "";
 
         try {
-            GitkitUser gitkitUser = null;
+            GitkitUser gitkitUser;
             GitkitClient gitkitClient = GitkitClient.createFromJson("gitkit-server-config.json");
 
-            Http.Request request = request();
+            gitkitUser = gitkitClient.validateToken(token);
+            if (gitkitUser == null)
+                return status(StatusCode.UnprocessableEntity);
 
-            gitkitUser = gitkitClient.validateToken(request().getHeader("gtoken"));
-            if (gitkitUser != null) {
-                userInfo = "Welcome back!<br><br> Email: " + gitkitUser.getEmail() + "<br> Id: "
-                        + gitkitUser.getLocalId() + "<br> Provider: " + gitkitUser.getCurrentProvider();
-            }
+//            userInfo = "Welcome back!<br><br> Email: "
+//                    + gitkitUser.getEmail() + "<br> Id: "
+//                    + gitkitUser.getLocalId()
+//                    + "<br> Provider: " + gitkitUser.getCurrentProvider();
+
+            userInfo = gitkitUser.getName();
+
         } catch (GitkitClientException | IOException e) {
             e.printStackTrace();
         }
