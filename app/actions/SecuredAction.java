0b6967649fac0b5ed3a4c9a76a577ba3d7d27129
Kasper Saaby <kdsaaby@gmail.com>
2016-02-29 16:08:53 +0100

    Another implementation of OAuth

--- app/actions/SecuredAction.java
+++ app/actions/SecuredAction.java
@@ -4,27 +4,28 @@ import play.libs.F;
 import play.mvc.Action;
 import play.mvc.Http;
 import play.mvc.Result;
+import play.mvc.Results;
+
+import static play.mvc.Controller.session;
 
 public class SecuredAction extends Action.Simple {
     @Override
     public F.Promise<Result> call(Http.Context ctx) throws Throwable {
-//        String token = getTokenFromHeader(ctx);
-//        if (token != null) {
-//            User user = BIConversion.User.find.where().eq("authToken", token).findUnique();
-//            if (user != null) {
-//                ctx.request().setUsername(user.username);
-//                return delegate.call(ctx);
-//            }
-//        }
-//        Result unauthorized = Results.unauthorized("unauthorized");
-//        return F.Promise.pure(unauthorized);
-        return delegate.call(ctx);
+        String receivedToken = getTokenFromHeader(ctx);
+        if (receivedToken != null) {
+            String username = session(receivedToken);
+            if (username != null) {
+                return delegate.call(ctx);
+            }
+        }
+        Result unauthorized = Results.unauthorized("unauthorized");
+        return F.Promise.pure(unauthorized);
     }
 
     private String getTokenFromHeader(Http.Context ctx) {
-        String[] authTokenHeaderValues = ctx.request().headers().get("X-AUTH-TOKEN");
+        String[] authTokenHeaderValues = ctx.request().headers().get("Authorization");
         if ((authTokenHeaderValues != null) && (authTokenHeaderValues.length == 1) && (authTokenHeaderValues[0] != null))
-            return authTokenHeaderValues[0];
+            return authTokenHeaderValues[0].substring("Bearer ".length());
 
         return null;
     }
--- app/controllers/auth.java
+++ app/controllers/auth.java
@@ -1,7 +1,10 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.api.client.auth.oauth2.*;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.*;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -9,19 +12,81 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import core.StatusCode;
 import play.Logger;
 import play.libs.Json;
+import play.mvc.Controller;
 import play.mvc.Result;
 
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.Collections;
 
-public class auth extends Application {
+public class auth extends Controller {
     private static final HttpTransport httpTransport = new NetHttpTransport();
 
     private static final String authorizationServerUrl = "https://github.com/login/oauth/authorize";
     private static final String tokenServerUrl = "https://github.com/login/oauth/access_token";
-    private static final String clientId = "e8c97a27f3858b4370ef";
+    private static final String clientId = "915544938368-etbmhsu4bk7illn6eriesf60v6q059kh.apps.googleusercontent.com";
     private static final String clientSecret = "3d45a6b5666c0f16d4fd04f3a2f03c9705f1da4f";
-    //private static final String callbackUrl = "http://localhost:9000/callback";
+
+    public Result verify(String tokenId) {
+        String accessToken = request().getHeader("Authorization");
+        String tokenPrefix = "Bearer ";
+
+        if (accessToken.startsWith(tokenPrefix) && accessToken.length() > tokenPrefix.length())
+            accessToken = accessToken.substring(tokenPrefix.length());
+
+        JsonFactory jsonFactory = new JacksonFactory();
+        HttpTransport transport = new NetHttpTransport();
+
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+                .setAudience(Collections.singletonList(clientId))
+                .setIssuer("accounts.google.com")
+                .build();
+
+        // (Receive idTokenString by HTTPS POST)
+        GoogleIdToken idToken;
+        try {
+            idToken = verifier.verify(tokenId);
+        } catch (GeneralSecurityException | IOException e) {
+            return status(StatusCode.UnprocessableEntity, e.toString());
+        } catch (IllegalArgumentException e) {
+            return status(StatusCode.UnprocessableEntity, e.toString());
+        }
+
+        if (idToken != null) {
+            JsonNode credential = makeCall(accessToken);
+
+            GoogleIdToken.Payload payload = idToken.getPayload();
+
+            String userId = payload.getSubject();
+            if (credential == null || !credential.get("sub").asText().equals(userId))
+                return status(StatusCode.UnprocessableEntity);
+
+            // Get profile information from payload
+            String email = payload.getEmail();
+            boolean emailVerified = payload.getEmailVerified();
+            String name = (String) payload.get("name");
+            String pictureUrl = (String) payload.get("picture");
+            String locale = (String) payload.get("locale");
+            String familyName = (String) payload.get("family_name");
+            String givenName = (String) payload.get("given_name");
+
+            ObjectNode node = Json.newObject();
+            node.put("userId", userId);
+            node.put("givenName", givenName);
+            node.put("familyName", familyName);
+            node.put("name", name);
+            node.put("email", email);
+            node.put("emailVerified", emailVerified);
+            node.put("locale", locale);
+            node.put("pictureUrl", pictureUrl);
+
+            session(accessToken, name);
+
+            return ok(node);
+        } else {
+            return status(StatusCode.UnprocessableEntity, "Invalid ID token.");
+        }
+    }
 
     public Result login() {
         String callbackUrl = controllers.routes.auth.callback().absoluteURL(request());
@@ -66,23 +131,39 @@ public class auth extends Application {
         return ok(node);
     }
 
-    public Result user() {
-        String accessToken = request().getQueryString("accessToken");
-        GenericUrl url = new GenericUrl("https://api.github.com/user");
+//    public Result user() {
+//        String accessToken = request().getQueryString("accessToken");
+//        GenericUrl url = new GenericUrl("https://www.googleapis.com/oauth2/v3/tokeninfo");
+//
+//        Credential credential = new Credential(BearerToken.queryParameterAccessMethod());
+//        credential.setAccessToken(accessToken);
+//        HttpRequestFactory requestFactory = httpTransport.createRequestFactory(credential::initialize);
+//
+//        String content;
+//        try {
+//            HttpRequest request = requestFactory.buildGetRequest(url);
+//            HttpResponse response = request.execute();
+//            content = response.parseAsString();
+//        } catch (IOException e) {
+//            return status(StatusCode.UnprocessableEntity, e.toString());
+//        }
+//
+//        return ok(content);
+//    }
 
-        Credential credential = new Credential(BearerToken.authorizationHeaderAccessMethod());
+    private JsonNode makeCall(String accessToken) {
+        GenericUrl url = new GenericUrl("https://www.googleapis.com/oauth2/v3/tokeninfo");
+
+        Credential credential = new Credential(BearerToken.queryParameterAccessMethod());
         credential.setAccessToken(accessToken);
         HttpRequestFactory requestFactory = httpTransport.createRequestFactory(credential::initialize);
 
-        String content;
         try {
             HttpRequest request = requestFactory.buildGetRequest(url);
             HttpResponse response = request.execute();
-            content = response.parseAsString();
+            return Json.parse(response.parseAsString());
         } catch (IOException e) {
-            return status(StatusCode.UnprocessableEntity, e.toString());
+            return null;
         }
-
-        return ok(content);
     }
 }
