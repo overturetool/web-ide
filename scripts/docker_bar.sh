ff8e63254dd37a96b70b456dc7ef6426d73e5dca
Kasper Saaby <kdsaaby@gmail.com>
2016-02-29 10:02:24 +0100

    More docker

--- scripts/docker_bar.sh
+++ scripts/docker_bar.sh
@@ -16,5 +16,6 @@ echo " - build finished"
 
 #docker exec -it overture_api bash
 #docker logs -f overture_api
-docker tag overture/api kdsaaby/overture_api
-docker push kdsaaby/overture_api
+
+#docker tag overture/api kdsaaby/overture_api
+#docker push kdsaaby/overture_api
--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -2,10 +2,17 @@
 
 # -m 128M seems to be the minimum
 
+#docker run -d \
+#    -p 9000:9000 \
+#    -m 128M \
+#    --memory-swap -1 \
+#    --env "JAVA_OPTS=-Xms64m -Xmx128m -Xss1M -XX:+CMSClassUnloadingEnabled" \
+#    --ulimit nofile=1024 \
+#    --name overture_api kdsaaby/overture_api
+
 docker run -d \
-    -p 9000:9000 \
-    -m 128M \
-    --memory-swap -1 \
-    --env "JAVA_OPTS=-Xms64m -Xmx128m -Xss1M -XX:+CMSClassUnloadingEnabled" \
+    -p $1:9000 \
+    --env "JAVA_OPTS=-Xms128M -Xmx256M -Xss1M -XX:+CMSClassUnloadingEnabled" \
+    -m 256M --memory-swap 512M \
     --ulimit nofile=1024 \
-    --name overture_api kdsaaby/overture_api
+    --name overture_api_$1 kdsaaby/overture_api
