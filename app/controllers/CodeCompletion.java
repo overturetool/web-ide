85cf03b187603e73941659fa9729c58589392fa1
Kasper Saaby <kdsaaby@gmail.com>
2016-08-08 15:51:19 +0200

    Renaming controller

--- app/controllers/CodeCompletion.java
+++ app/controllers/CodeCompletion.java
@@ -16,8 +16,8 @@ import play.mvc.Result;
 
 import java.util.List;
 
-public class CC extends Application {
-    private final Logger logger = LoggerFactory.getLogger(CC.class);
+public class CodeCompletion extends Application {
+    private final Logger logger = LoggerFactory.getLogger(CodeCompletion.class);
 
     public Result proposal(String account, String path) {
         String lineStr = request().getQueryString("line");
